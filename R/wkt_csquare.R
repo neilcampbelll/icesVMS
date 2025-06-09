44da62565ee3995c8685ee9f3aacbb5b2929e133
Neil Campbell <neil.campbelll@gmail.com>
2025-06-09 16:14:33 +0200

    Update wkt_csquare.R
    made the wkt_csquare function work with other sized c-squares

--- R/wkt_csquare.R
+++ R/wkt_csquare.R
@@ -3,24 +3,25 @@
 #' utility to build a WKT (Well Known Text) polygon string
 #' for a c-square
 #'
-#' @param lat latitude
-#' @param lon longitude
+#' @param lat latitude (decimal degrees)
+#' @param lon longitude (decimal degrees)
+#' @param size dimension of c-square (decimal degrees)
 #'
 #' @return a string in WKT format
 #'
 #' @examples
 #'
-#' wkt_csquare(55, 0.1)
+#' wkt_csquare(55, 0.1, 0.05)
 #'
 #' @importFrom glue glue
 #'
 #' @export
-wkt_csquare <- function(lat, lon) {
-  glue(
-    "POLYGON(({lon - 0.025} {lat - 0.025},",
-    "{lon - 0.025} {lat + 0.025},",
-    "{lon + 0.025} {lat + 0.025},",
-    "{lon + 0.025} {lat - 0.025},",
-    "{lon - 0.025} {lat - 0.025}))"
-  )
+
+wkt_csquare_flexible <- function(lat, lon, size = 0.05) {
+    half_size <- size / 2
+    glue("POLYGON(({lon - half_size} {lat - half_size},", 
+         "{lon - half_size} {lat + half_size},", 
+         "{lon + half_size} {lat + half_size},", 
+         "{lon + half_size} {lat - half_size},", 
+         "{lon - half_size} {lat - half_size}))")
 }
